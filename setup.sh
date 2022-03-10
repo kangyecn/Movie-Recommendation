e9a6d4f8eb0450e812bfc1213ff8a7d13edc69ac
kangyecn <93654227+kangyecn@users.noreply.github.com>
2022-03-10 19:18:37 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "
-[server]\n
-port = $PORT\n
-enableCORS = false\n
-headless = true\n
-\n
-" > ~/.streamlit/config.toml
+echo "\
+[general]\n\
+email = \"kangyecn@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+">~/.streamlit/config.toml
+
+echo $PORT 
